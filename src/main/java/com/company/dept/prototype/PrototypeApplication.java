ef657cc8f1bb3397528d0c97d6d7bf7c76c44bec
Shiva <shiva@Vidyas-Mac-mini.fios-router.home>
2018-05-02 22:00:01 -0400

    initial cut

---
+++ src/main/java/com/company/dept/prototype/PrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.company.dept.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApplication.class, args);
+		System.out.println("234");
+	}
+}
